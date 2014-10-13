4b8aa1685bec0d998ef0d961e34de692600c79af
wkorando <wkorando@gmail.com>
2014-10-13 17:41:45 -0500

    Implemented reflection... went easier than expected.

--- dealership/src/main/java/com/dealership/report/DealershipReportBuilder.java
+++ dealership/src/main/java/com/dealership/report/DealershipReportBuilder.java
@@ -1,35 +1,87 @@
 package com.dealership.report;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 import com.dealership.model.Automobile;
 
 public class DealershipReportBuilder {
-	public Set<Automobile> sortAutomobiles(String fieldToOrderBy, SortOrder sortOrder, Set<Automobile> automobiles) {
-		Object[] array = sort(automobiles.toArray());
-		Set<Automobile> automobiles2 = new HashSet<Automobile>();
+	private Logger LOG = Logger.getLogger(DealershipReportBuilder.class);
+
+	public List<Automobile> sortAutomobiles(String fieldToOrderBy,
+			SortOrder sortOrder, Set<Automobile> automobiles) {
+		Method method = findMethod(fieldToOrderBy, automobiles.toArray()[0]);
+		Object[] array = sort(automobiles.toArray(), method);
+		List<Automobile> automobiles2 = new ArrayList<Automobile>();
 		for (int i = 0; i < array.length; i++) {
 			automobiles2.add((Automobile) array[i]);
 		}
 		return automobiles2;
 	}
 
-	private Object[] sort(Object[] automobiles) {
+	private Method findMethod(String fieldToOrderBy, Object object) {
+		Method method = null;
+		String methodName = buildMethodName(fieldToOrderBy);
+		try {
+			method = object.getClass().getMethod(methodName, new Class[0]);
+		} catch (NoSuchMethodException e) {
+			LOG.error("An error occurred while trying to find method: "
+					+ methodName, e);
+			throw new RuntimeException(
+					"An error occurred while trying to find method: "
+							+ methodName, e);
+		}
+
+		return method;
+	}
+
+	private String buildMethodName(String fieldToOrderBy) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("get");
+		builder.append(fieldToOrderBy);
+		return builder.toString();
+	}
+
+	private Object[] sort(Object[] automobiles, Method method) {
 		boolean sortedItem = false;
 		for (int i = 0; i < automobiles.length - 1; i++) {
+
 			Automobile first = (Automobile) automobiles[i];
 			Automobile second = (Automobile) automobiles[i + 1];
+			Comparable<Object> firstComparable = getVal(method, first);
+			Comparable<Object> secondComparable = getVal(method, second);
 
-			if (first.getPrice().compareTo(second.getPrice()) < 0) {
+			if (firstComparable.compareTo(secondComparable) > 0) {
 				automobiles[i] = second;
 				automobiles[i + 1] = first;
 				sortedItem = true;
 			}
 		}
 		if (sortedItem) {
-			sort(automobiles);
+			sort(automobiles, method);
 		}
 		return automobiles;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Comparable<Object> getVal(Method method, Automobile first) {
+		Comparable<Object> firstComparable = null;
+		try {
+			firstComparable = (Comparable<Object>) method.invoke(first,
+					new Object[0]);
+
+		} catch (ReflectiveOperationException e) {
+			LOG.error("An error occurred while attempting to invoke method: "
+					+ method.getName(), e);
+			throw new RuntimeException(
+					"An error occurred while attempting to invoke method: "
+							+ method.getName(), e);
+		}
+		return firstComparable;
 	}
 }
--- dealership/src/test/java/com/dealership/report/TestDealershipReportBuilder.java
+++ dealership/src/test/java/com/dealership/report/TestDealershipReportBuilder.java
@@ -12,8 +12,9 @@ import com.dealership.model.Truck;
 
 public class TestDealershipReportBuilder {
 	private Logger LOG = Logger.getLogger(TestDealershipReportBuilder.class);
+
 	@Test
-	public void testSortAutomobiles(){
+	public void testSortAutomobilesByPrice() {
 		Truck truck1 = new Truck();
 		truck1.setPrice(new BigDecimal(100));
 		Truck truck2 = new Truck();
@@ -21,9 +22,9 @@ public class TestDealershipReportBuilder {
 		Truck truck3 = new Truck();
 		truck3.setPrice(new BigDecimal(700));
 		Truck truck4 = new Truck();
-		truck4.setPrice(new BigDecimal(1));
+		truck4.setPrice(new BigDecimal(600));
 		Truck truck5 = new Truck();
-		truck5.setPrice(new BigDecimal(600));
+		truck5.setPrice(new BigDecimal(1));
 		Set<Automobile> automobiles = new HashSet<Automobile>();
 		truck1.equals(truck5);
 		automobiles.add(truck1);
@@ -31,11 +32,38 @@ public class TestDealershipReportBuilder {
 		automobiles.add(truck3);
 		automobiles.add(truck4);
 		automobiles.add(truck5);
-		
-		DealershipReportBuilder  reportBuilder = new DealershipReportBuilder();
-		for(Automobile automobile :reportBuilder.sortAutomobiles("", SortOrder.ASCENDING, automobiles)){
-			System.out.println(automobile.getPrice());
+
+		DealershipReportBuilder reportBuilder = new DealershipReportBuilder();
+		for (Automobile automobile : reportBuilder.sortAutomobiles("Price",
+				SortOrder.ASCENDING, automobiles)) {
+			LOG.debug(automobile.getPrice());
 		}
-		
+	}
+
+	@Test
+	public void testSortTrucksByTowingCapacity() {
+		Truck truck1 = new Truck();
+		truck1.setTowingCapacity(20000);
+		Truck truck2 = new Truck();
+		truck2.setTowingCapacity(11500);
+		Truck truck3 = new Truck();
+		truck3.setTowingCapacity(25000);
+		Truck truck4 = new Truck();
+		truck4.setTowingCapacity(5000);
+		Truck truck5 = new Truck();
+		truck5.setTowingCapacity(8000);
+		Set<Automobile> automobiles = new HashSet<Automobile>();
+		truck1.equals(truck5);
+		automobiles.add(truck1);
+		automobiles.add(truck2);
+		automobiles.add(truck3);
+		automobiles.add(truck4);
+		automobiles.add(truck5);
+
+		DealershipReportBuilder reportBuilder = new DealershipReportBuilder();
+		for (Automobile automobile : reportBuilder.sortAutomobiles("TowingCapacity",
+				SortOrder.ASCENDING, automobiles)) {
+			LOG.debug(((Truck)automobile).getTowingCapacity());
+		}
 	}
 }
